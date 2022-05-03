3f746dacab022d7da4ed4a2712481fbce3ee8dc8
milkyklim <10698619+milkyklim@users.noreply.github.com>
2022-05-03 21:23:52 +0200

    refactor: remove dead code

--- yearn_gas/populate_db.py
+++ yearn_gas/populate_db.py
@@ -18,9 +18,8 @@ def main():
                 with gzip.open(f"{DATAFOLDER}/{filename}", "rt") as f:
                     tsv_file = csv.DictReader(f, delimiter="\t")
                     for block in tsv_file:
-
                         if not db.Block.exists(id=block["id"]):
-                            fixy = [
+                            fix_keys = [
                                 "hash",
                                 "miner",
                                 "extra_data_hex",
@@ -32,47 +31,10 @@ def main():
                                 "transactions_root",
                             ]
 
-                            for f in fixy:
-                                block[f] = block[f].encode()
+                            for k in fix_keys:
+                                block[k] = block[k].encode()
 
                             db.Block(**block)
-
-                            # block = db.Block(
-                            #     id=line["id"],
-                            #     hash=line["hash"].encode(),
-                            #     time=line["time"],
-                            #     size=line["size"],
-                            #     miner=line["miner"].encode(),
-                            #     extra_data_hex=line["extra_data_hex"].encode(),
-                            #     difficulty=line["difficulty"],
-                            #     gas_used=line["gas_used"],
-                            #     gas_limit=line["gas_limit"],
-                            #     logs_bloom=line["logs_bloom"].encode(),
-                            #     mix_hash=line["mix_hash"].encode(),
-                            #     nonce=line["nonce"],
-                            #     receipts_root=line["receipts_root"].encode(),
-                            #     sha3_uncles=line["sha3_uncles"].encode(),
-                            #     state_root=line["state_root"].encode(),
-                            #     total_difficulty=line["total_difficulty"],
-                            #     transactions_root=line["transactions_root"].encode(),
-                            #     uncle_count=line["uncle_count"],
-                            #     transaction_count=line["transaction_count"],
-                            #     synthetic_transaction_count=line["synthetic_transaction_count"],
-                            #     call_count=line["call_count"],
-                            #     synthetic_call_count=line["synthetic_call_count"],
-                            #     value_total=line["value_total"],
-                            #     value_total_usd=line["value_total_usd"],
-                            #     internal_value_total=line["internal_value_total"],
-                            #     internal_value_total_usd=line["internal_value_total_usd"],
-                            #     generation=line["generation"],
-                            #     generation_usd=line["generation_usd"],
-                            #     uncle_generation=line["uncle_generation"],
-                            #     uncle_generation_usd=line["uncle_generation_usd"],
-                            #     fee_total=line["fee_total"],
-                            #     fee_total_usd=line["fee_total_usd"],
-                            #     reward=line["reward"],
-                            #     reward_usd=line["reward_usd"],
-                            # )
 
 
 if __name__ == "__main__":
